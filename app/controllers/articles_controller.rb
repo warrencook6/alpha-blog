78f3ec6b95534e4c7e7b8b100d89346c781d4003
Warren <warren.cook6@gmail.com>
2018-01-24 16:45:09 -0800

    Add edit, updated, all articles and i think that is it

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,26 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
+  end
+
+  #edit Article
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Your article has been updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
   end
 
   #Creates the article
