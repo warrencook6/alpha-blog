33fb2370d9d603df60bde060d2d42207aa066b2b
Warren <warren.cook6@gmail.com>
2018-02-07 15:20:50 -0800

    Users are dank, only article owner can edit articles, when you click on an article it shows the user and has links to that users profile and stuff

--- Gemfile
+++ Gemfile
@@ -54,7 +54,7 @@ group :development do
 end
 
 group :production do
-  gem 'pg'
+  gem 'pg', '~> 0.11'
   gem 'rails_12factor'
 end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
@@ -6,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the Alpha Blog #{@user.username}"
       redirect_to user_path(@user)
     else
@@ -32,8 +38,13 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
-    params.require(:user).permit(:username, :emai, :password)
+    params.require(:user).permit(:username, :email, :password)
   end
 
 end
